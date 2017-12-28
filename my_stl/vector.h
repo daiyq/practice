a349c5d20d278282439d07c8c6d39e40961697d6
Dai Yuanqiao <daiyuanqiao10@gmail.com>
2017-12-28 11:07:58 +0800

    Add files via upload

--- my_stl/vector.h
+++ my_stl/vector.h
@@ -5,9 +5,10 @@
 #include <cstddef> //ptrdiff_t, size_t
 #include <stdexcept> //throw
 #include <initializer_list>
-#include <algorithm>
+#include <type_traits>
+//#include <algorithm>
 #include "iterator.h"
-#include <iterator>
+//#include <iterator>
 #include "memory.h"
 #include "algorithm.h"
 
@@ -38,7 +39,7 @@ namespace d_stl {
 
 		vector();
 		explicit vector(size_type count);
-		vector(size_type count, const T& value);
+		vector(size_type count, const value_type& value);
 		template<class InputIt>
 		vector(InputIt first, InputIt last);
 		vector(const vector& other);
@@ -51,7 +52,7 @@ namespace d_stl {
 		vector& operator=(vector&& other);
 		vector& operator=(std::initializer_list<T> init);
 
-		void assign(size_type count, const T& value);
+		void assign(size_type count, const value_type& value);
 		template<class InputIt>
 		void assign(InputIt first, InputIt last);
 		void assign(std::initializer_list<T> init);
@@ -147,16 +148,16 @@ namespace d_stl {
 		void shrink_to_fit(); //removed unused capacity 
 
 		void clean();
-		iterator insert(const_iterator pos, const T& value); // return the first element inserted
-		iterator insert(const_iterator pos, T&& value);
-		iterator insert(const_iterator pos, size_type count, const T& value);
+		iterator insert(const_iterator pos, const value_type& value); // return the first element inserted
+		iterator insert(const_iterator pos, value_type&& value);
+		iterator insert(const_iterator pos, size_type count, const value_type& value);
 		template<class InputIt>
 		iterator insert(const_iterator pos, InputIt first, InputIt last);
 		iterator insert(const_iterator pos, std::initializer_list<T> init);
 		iterator erase(const_iterator pos); //return the following removed element
 		iterator erase(const_iterator first, const_iterator last);
-		void push_back(const T& value);
-		void push_back(T&& value);
+		void push_back(const value_type& value);
+		void push_back(value_type&& value);
 		void pop_back();
 		void resize(size_type count);
 		void resize(size_type count, const value_type& value);
@@ -176,6 +177,18 @@ namespace d_stl {
 		void destory_and_reallocate(size_type need_size);
 		void copy_and_reallocate(size_type need_size);
 
+		
+		void vector_base(size_type count, const value_type& value, std::true_type);
+		template<class InputIt>
+		void vector_base(InputIt first, InputIt last, std::false_type);
+		
+		void assign_base(size_type count, const value_type& value, std::true_type);
+		template<class InputIt>
+		void assign_base(InputIt first, InputIt last, std::false_type);
+		
+		iterator insert_base(const_iterator pos, size_type count, const value_type& value, std::true_type);
+		template<class InputIt>
+		iterator insert_base(const_iterator pos, InputIt first, InputIt last, std::false_type);
 	};
 
 	//错误地点，两个模板参数都要写全，即使是有默认模板参数，也要写全！！
@@ -193,21 +206,14 @@ namespace d_stl {
 	}
 
 	template<class T, class Allocator>
-	vector<T, Allocator>::vector(size_type count, const T& value) {
-		std::printf("number and value\n");
-		ininitialized(count);
-		uninitialized_fill_n(begin_, count, value);
+	vector<T, Allocator>::vector(size_type count, const value_type& value) {	
+		vector_base(count, value, typename std::is_integral<size_type>::type());
 	}
 	
 	template<class T, class Allocator>
 	template<class InputIt>
 	vector<T, Allocator>::vector(InputIt first, InputIt last) {
-		std::printf("Iterator\n");
-		/*
-		size_type count = static_cast<size_type>(distance(first, last));
-		ininitialized(count);
-		uninitialized_copy(first, last, begin_);
-		*/
+		vector_base(first, last, typename std::is_integral<InputIt>::type());
 	}
 	
 	template<class T, class Allocator>
@@ -279,29 +285,14 @@ namespace d_stl {
 	}
 
 	template<class T, class Allocator>
-	void vector<T, Allocator>::assign(size_type count, const T& value) {
-		if (capacity() < count) {
-			destory_and_reallocate(count);
-		}
-		else {
-			destory(begin_, end_);
-			end_ = begin_ + count;
-		}
-		uninitialized_fill_n(begin_, count, value);
+	void vector<T, Allocator>::assign(size_type count, const value_type& value) {
+		assign_base(count, value, typename std::is_integral<size_type>::type());
 	}
 
 	template<class T, class Allocator>
 	template<class InputIt>
 	void vector<T, Allocator>::assign(InputIt first, InputIt last) {
-		size_type count = static_cast<size_type>(distance(first, last));
-		if (capacity() < count) {
-			destory_and_reallocate(count);
-		}
-		else {
-			destory(begin_, end_);
-			end_ = begin_ + count;
-		}
-		uninitialized_copy(first, last, begin_);
+		assign_base(first, last, typename std::is_integral<InputIt>::type());
 	}
 
 	template<class T, class Allocator>
@@ -344,9 +335,11 @@ namespace d_stl {
 
 	//insert, push, pop
 	template<class T, class Allocator>
-	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, const T& value) {
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, const value_type& value) {
+		insert_base(pos, 1, value, typename std::is_integral<size_type>::type());
+		/*
 		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
-		if (end_ = stroage_) {
+		if (end_ == stroage_) {
 			size_type new_size = 2 * size();
 			T* t_begin_ = allocate(new_size);
 			T* t_end_ = t_begin_ + size() + 1;
@@ -374,13 +367,16 @@ namespace d_stl {
 			end_++;
 			return iterator(begin_ + dis);
 		}
+		*/
 	}
 
+	//the using of rvalue reference
+	//should not be treated as lvalue reference
 	template<class T, class Allocator>
-	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, T&& value) {
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, value_type&& value) {
 		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
-		if (end_ = stroage_) {
-			size_type new_size = 2 * size();
+		if (end_ == stroage_) {
+			size_type new_size = size() == 0 ? 2 : size() * 2;
 			T* t_begin_ = allocate(new_size);
 			T* t_end_ = t_begin_ + size() + 1;
 			T* t_stroage_ = t_begin_ + (((sizeof(T)*new_size + 7) / 8) * 8) / sizeof(T);
@@ -402,7 +398,7 @@ namespace d_stl {
 			else {
 				uninitialized_copy(end_ - 1, end_, end_ + 1);
 				copy_backward(begin_ + dis, end_ - 1, end_);
-				*(begin_ + dis) = value;
+				*(begin_ + dis) = value; //move assignment
 			}
 			end_++;
 			return iterator(begin_ + dis);
@@ -410,80 +406,20 @@ namespace d_stl {
 	}
 
 	template<class T, class Allocator>
-	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, size_type count, const T& value) {
-		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
-		if (end_ + count > stroage_) {
-			size_type new_size = size() > count ? 2 * size() : 2 * count;
-			T* t_begin_ = allocate(new_size);
-			T* t_end_ = t_begin_ + size() + count;
-			T* t_stroage_ = t_begin_ + (((sizeof(T)*new_size + 7) / 8) * 8) / sizeof(T);
-
-			uninitialized_copy(cbegin(), pos, t_begin_);
-			uninitialized_fill_n(t_begin_ + dis, count, value);
-			uninitialized_copy(pos, cend(), t_begin_ + dis + count);
-
-			delete_data_and_memory();
-			begin_ = t_begin_;
-			end_ = t_end_;
-			stroage_ = t_stroage_;
-			return iterator(begin_ + dis);
-		}
-		else {
-			//different area
-			if ((size() - dis) > count) {
-				uninitialized_copy(end_ - count, end_, end_);
-				copy_backward(begin_ + dis, end_ - count, end_);
-				destory(begin_ + dis, begin_ + dis + count);
-			}
-			else {
-				uninitialized_copy(begin_ + dis, end_, begin_ + dis + count);
-				destory(begin_ + dis, end_);
-			}
-			uninitialized_fill_n(begin_ + dis, count, value);
-			end_ += count;
-			return iterator(begin_ + dis);
-		}
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, size_type count, const value_type& value) {
+		insert_base(pos, count, value, typename std::is_integral<size_type>::type());	
 	}
 
 	template<class T, class Allocator>
 	template<class InputIt>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, InputIt first, InputIt last) {
-		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
-		size_type count = static_cast<size_type>(distance(first, last));
-		if (end_ + count > stroage_) {
-			size_type new_size = size() > count ? 2 * size() : 2 * count;
-			T* t_begin_ = allocate(new_size);
-			T* t_end_ = t_begin_ + size() + count;
-			T* t_stroage_ = t_begin_ + (((sizeof(T)*new_size + 7) / 8) * 8) / sizeof(T);
-
-			uninitialized_copy(cbegin(), pos, t_begin_);
-			uninitialized_copy(first, last, t_begin_ + dis);
-			uninitialized_copy(pos, cend(), t_begin_ + dis + count);
-
-			delete_data_and_memory();
-			begin_ = t_begin_;
-			end_ = t_end_;
-			stroage_ = t_stroage_;
-			return iterator(begin_ + dis);
-		}
-		else {
-			if ((size() - dis) > count) {
-				uninitialized_copy(end_ - count, end_, end_);
-				copy_backward(begin_ + dis, begin_ + dis + count, end_);
-				destory(begin_ + dis, begin_ + dis + count);
-			}
-			else {
-				uninitialized_copy(begin_ + dis, end_, begin_ + dis + count);
-				destory(begin_ + dis, end_);
-			}
-			uninitialized_copy(first, last, begin_ + dis);
-			end_ += count;
-			return iterator(begin_ + dis);
-		}
+		insert_base(pos, first, last, typename std::is_integral<InputIt>::type());
 	}
 
 	template<class T, class Allocator>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator pos, std::initializer_list<T> init) {
+		insert_base(pos, init.begin(), init.end(), typename std::is_integral<const_iterator>::type());
+		/*
 		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
 		size_type count = init.size();
 		if (end_ + count > stroage_) {
@@ -516,6 +452,7 @@ namespace d_stl {
 			end_ += count;
 			return iterator(begin_ + dis);
 		}
+		*/
 	}
 
 	template<class T, class Allocator>
@@ -532,7 +469,7 @@ namespace d_stl {
 	}
 
 	template<class T, class Allocator>
-	void vector<T, Allocator>::push_back(const T& value) {
+	void vector<T, Allocator>::push_back(const value_type& value) {
 		size_type old_size = size();
 		if (end_ == stroage_) {
 			size_type dis_size = old_size == 0 ? 1 : old_size;
@@ -544,7 +481,7 @@ namespace d_stl {
 	}
 
 	template<class T, class Allocator>
-	void vector<T, Allocator>::push_back(T&& value) {
+	void vector<T, Allocator>::push_back(value_type&& value) {
 		size_type old_size = size();
 		if (end_ == stroage_) {
 			size_type dis_size = old_size == 0 ? 1 : old_size;
@@ -652,6 +589,122 @@ namespace d_stl {
 		begin_ = t_begin_;
 		stroage_ = t_stroage_;
 	}
+
+	template<class T, class Allocator>
+	void vector<T, Allocator>::vector_base(size_type count, const value_type& value, std::true_type) {
+		std::printf("number and value\n");
+		ininitialized(count);
+		uninitialized_fill_n(begin_, count, value);
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	void vector<T, Allocator>::vector_base(InputIt first, InputIt last, std::false_type) {
+		std::printf("Iterator\n");
+		size_type count = static_cast<size_type>(distance(first, last));
+		ininitialized(count);
+		uninitialized_copy(first, last, begin_);
+	}
+
+	template<class T, class Allocator>
+	void vector<T, Allocator>::assign_base(size_type count, const value_type& value, std::true_type) {
+		if (capacity() < count) {
+			destory_and_reallocate(count);
+		}
+		else {
+			destory(begin_, end_);
+			end_ = begin_ + count;
+		}
+		uninitialized_fill_n(begin_, count, value);
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	void vector<T, Allocator>::assign_base(InputIt first, InputIt last, std::false_type) {
+		size_type count = static_cast<size_type>(distance(first, last));
+		if (capacity() < count) {
+			destory_and_reallocate(count);
+		}
+		else {
+			destory(begin_, end_);
+			end_ = begin_ + count;
+		}
+		uninitialized_copy(first, last, begin_);
+	}
+
+	template<class T, class Allocator>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert_base(const_iterator pos, size_type count, const value_type& value, std::true_type) {
+		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
+		if (end_ + count > stroage_) {
+			size_type new_size = size() > count ? 2 * size() : 2 * count;
+			T* t_begin_ = allocate(new_size);
+			T* t_end_ = t_begin_ + size() + count;
+			T* t_stroage_ = t_begin_ + (((sizeof(T)*new_size + 7) / 8) * 8) / sizeof(T);
+
+			uninitialized_copy(cbegin(), pos, t_begin_);
+			uninitialized_fill_n(t_begin_ + dis, count, value);
+			uninitialized_copy(pos, cend(), t_begin_ + dis + count);
+
+			delete_data_and_memory();
+			begin_ = t_begin_;
+			end_ = t_end_;
+			stroage_ = t_stroage_;
+			return iterator(begin_ + dis);
+		}
+		else {
+			//different area
+			if ((size() - dis) > count) {
+				uninitialized_copy(end_ - count, end_, end_);
+				copy_backward(begin_ + dis, end_ - count, end_);
+				destory(begin_ + dis, begin_ + dis + count);
+			}
+			else {
+				uninitialized_copy(begin_ + dis, end_, begin_ + dis + count);
+				destory(begin_ + dis, end_);
+			}
+			uninitialized_fill_n(begin_ + dis, count, value);
+			end_ += count;
+			return iterator(begin_ + dis);
+		}
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert_base(const_iterator pos, InputIt first, InputIt last, std::false_type) {
+		size_type dis = static_cast<size_type>(distance(cbegin(), pos));
+		size_type count = static_cast<size_type>(distance(first, last));
+		if (end_ + count > stroage_) {
+			size_type new_size = size() > count ? 2 * size() : 2 * count;
+			T* t_begin_ = allocate(new_size);
+			T* t_end_ = t_begin_ + size() + count;
+			T* t_stroage_ = t_begin_ + (((sizeof(T)*new_size + 7) / 8) * 8) / sizeof(T);
+
+			uninitialized_copy(cbegin(), pos, t_begin_);
+			uninitialized_copy(first, last, t_begin_ + dis);
+			uninitialized_copy(pos, cend(), t_begin_ + dis + count);
+
+			delete_data_and_memory();
+			begin_ = t_begin_;
+			end_ = t_end_;
+			stroage_ = t_stroage_;
+			return iterator(begin_ + dis);
+		}
+		else {
+			if ((size() - dis) > count) {
+				uninitialized_copy(end_ - count, end_, end_);
+				copy_backward(begin_ + dis, begin_ + dis + count, end_);
+				destory(begin_ + dis, begin_ + dis + count);
+			}
+			else {
+				uninitialized_copy(begin_ + dis, end_, begin_ + dis + count);
+				destory(begin_ + dis, end_);
+			}
+			uninitialized_copy(first, last, begin_ + dis);
+			end_ += count;
+			return iterator(begin_ + dis);
+		}
+	}
+
 
 
 	template<class T, class Allocator = d_stl::allocator<T>>
