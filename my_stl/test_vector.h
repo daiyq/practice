3de518a4f6cb0d254995763e41dd3dc1873105e1
Dai Yuanqiao <daiyuanqiao10@gmail.com>
2018-01-04 17:02:45 +0800

    Add files via upload

--- my_stl/test_vector.h
+++ my_stl/test_vector.h
@@ -1,44 +1,38 @@
 #ifndef D_TEST_VECTOR_H_
 #define D_TEST_VECTOR_H_
 
-#include <cstdio> //printf
-#include <vector>
+#include "test.h"
 #include "vector.h"
+#include <vector>
+#include <cstdio> //printf
+#include <cassert>
 
 namespace d_stl {
-
-	static int test_vector_count = 0;
-	static int test_vector_pass = 0;
-
-#define EXPECT_EQ_BASE(equality, from_my, from_std, format) \
-	do{\
-		test_vector_count++;\
-		if (equality){\
-			test_vector_pass++;}\
-		else{\
-			std::printf("%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, from_my, from_std);}\
-	} while (0)
 
 	void test_vector_constructor() {
 		d_stl::vector<int> my_vector_1;
 		std::vector<int> s_vector_1;
+		assert(equal_containter(my_vector_1, s_vector_1));
 
 		d_stl::vector<int> my_vector_2(5);
 		std::vector<int> s_vector_2(5);
+		assert(equal_containter(my_vector_2, s_vector_2));
 
 		d_stl::vector<int> my_vector_3(5, 1);
 		std::vector<int> s_vector_3(5, 1);
+		assert(equal_containter(my_vector_3, s_vector_3));
 
 		std::vector<int> for_iterator(5, 2);
 		d_stl::vector<int> my_vector_4(for_iterator.begin(), for_iterator.end());
 		std::vector<int> s_vector_4(for_iterator.begin(), for_iterator.end());
+		assert(equal_containter(my_vector_4, s_vector_4));
 
 		d_stl::vector<int> for_lvalue1(5, 3);
 		std::vector<int> for_lvalue2(5, 3);
 		d_stl::vector<int> my_vector_5(for_lvalue1);
 		std::vector<int> s_vector_5(for_lvalue2);
+		assert(equal_containter(my_vector_5, s_vector_5));
 
-				
 	}
 
 	void test_vector_size() {
