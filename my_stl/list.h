4d07f7f98d8592e443b7434bb5967c1573383f3f
Dai Yuanqiao <daiyuanqiao10@gmail.com>
2018-01-07 21:48:32 +0800

    Add files via upload

--- my_stl/list.h
+++ my_stl/list.h
@@ -14,19 +14,29 @@
 
 namespace d_stl {
 
+	template<class T>
+	class ListNode {
+	public:
+		T* prev;
+		T* next;
+		T data;
+	};
+
 	template<class ListItem>
 	class ListIterator {
-	private:
+	public:
+		using iterator_category = bidirectional_iterator_tag;
+		using value_type = ListItem;
+		using difference_type = std::ptrdiff_t;
+		using pointer = ListItem*;
+		using reference = ListItem&;
+		using self = ListIterator<ListItem>;
+		using ptr_node = ListNode<ListItem>*;
 
+	private:
+		ptr_node current;
 
 	public:
-		typedef bidirectional_iterator_tag iterator_category;
-		typedef ListItem                   value_type;
-		typedef std::ptrdiff_t             difference_type;
-		typedef ListItem*                  pointer;
-		typedef ListItem&                  reference;
-		typedef ListIterator<ListItem> self;
-
 		reference operator*() const {
 			
 		}
@@ -81,7 +91,7 @@ namespace d_stl {
 		list(InputIt first, InputIt last);
 		list(const list& other);
 		list(list&& other);
-		list(std::initializer_list<T> init);
+		list(std::initializer_list<T> ilist);
 		~list();
 
 		list& operator=(const list& other);
@@ -93,10 +103,17 @@ namespace d_stl {
 		void assign(InputIt first, InputIt last);
 		void assign(std::initializer_list<T> ilist);
 
-		reference front();
-		const_reference front() const;
-		reference back();
-		const_reference back() const;
+		reference front() {
+		}
+
+		const_reference front() const {
+		}
+
+		reference back() {
+		}
+
+		const_reference back() const {
+		}
 
 		//Iterators
 		iterator begin() noexcept {
@@ -194,6 +211,213 @@ namespace d_stl {
 		void sort(Compare comp);
 
 	};
+
+	template<class T,class Allocator>
+	list<T, Allocator>::list() {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::list(size_type count) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::list(size_type count, const value_type& value) {
+	
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	list<T, Allocator>::list(InputIt first, InputIt last) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::list(const list& other) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::list(list&& other) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::list(std::initializer_list<T> ilist) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>::~list() {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>& list<T, Allocator>::operator=(const list& other) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>& list<T, Allocator>::operator=(list&& other) {
+	}
+
+	template<class T, class Allocator>
+	list<T, Allocator>& list<T, Allocator>::operator=(std::initializer_list<T> ilist) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::assign(size_type count, const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	void list<T, Allocator>::assign(InputIt first, InputIt last) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::assign(std::initializer_list<T> ilist) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::clear() {
+
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::insert(const_iterator pos, const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::insert(const_iterator pos, value_type&& value) {
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::insert(const_iterator pos, size_type count, const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	template<class InputIt>
+	typename list<T, Allocator>::iterator list<T, Allocator>::insert(const_iterator pos, InputIt first, InputIt last) {
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::insert(const_iterator pos, std::initializer_list<T> ilist) {
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::erase(const_iterator pos) {
+	}
+
+	template<class T, class Allocator>
+	typename list<T, Allocator>::iterator list<T, Allocator>::erase(const_iterator first, const_iterator last) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::push_back(const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::push_back(value_type&& value) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::pop_back() {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::push_front(const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::push_front(value_type&& value) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::pop_front() {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::resize(size_type count) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::resize(size_type count, const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::swap(list& other) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::merge(list& other) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::merge(list&& other) {
+	}
+
+	template<class T, class Allocator>
+	template<class Compare>
+	void list<T, Allocator>::merge(list& other, Compare comp) {
+	}
+
+	template<class T, class Allocator>
+	template<class Compare>
+	void list<T, Allocator>::merge(list&& other, Compare comp) {
+	}
+	
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list& other) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list&& other) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list& other, const_iterator it) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list&& other, const_iterator it) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list& other, const_iterator first, const_iterator last) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::splice(const_iterator pos, list&& other, const_iterator first, const_iterator last) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::remove(const value_type& value) {
+	}
+
+	template<class T, class Allocator>
+	template<class UnaryPredicate>
+	void list<T, Allocator>::remove_if(UnaryPredicate p) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::reverse() {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::unipue() {
+	}
+
+	template<class T, class Allocator>
+	template<class BinaryPredicate>
+	void list<T, Allocator>::unique(BinaryPredicate p) {
+	}
+
+	template<class T, class Allocator>
+	void list<T, Allocator>::sort() {
+	}
+
+	template<class T, class Allocator>
+	template<class Compare>
+	void list<T, Allocator>::sort(Compare comp) {
+	}
+
+
+
+
+
 
 	template<class T, class Allocator = d_stl::allocator<T>>
 	bool operator==(const list<T, Allocator>& lhs, const list<T, Allocator>& rhs) {
